87eba86e72d432905d6ede2a6e2836fbb2fedcb3
venkatesh <venkatesh.n@techtreeit.com>
2019-08-07 18:09:49 +0530

    profile initial commit

--- app/src/main/java/com/example/instagram/profile/ProfileActivity.java
+++ app/src/main/java/com/example/instagram/profile/ProfileActivity.java
@@ -1,9 +1,11 @@
 package com.example.instagram.profile;
 
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,12 +15,15 @@ import com.example.instagram.utils.BottomNavigationViewHelper;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityHomeBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_profile);
-       setUpNavigation();
+        ActivityHomeBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
+        setUpNavigation();
+        setUpToolBar();
     }
 
     private void setUpNavigation() {
@@ -27,5 +32,28 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu = navigationView.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    private void setUpToolBar() {
+        Toolbar toolbar = findViewById(R.id.profile_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profile_menu:
+                        Log.d(TAG, "onMenuItemClick: clicked");
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
